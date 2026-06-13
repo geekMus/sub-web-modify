0ebb06261e919009d026e18429b8654728e0fd19
Mus <geekMus@qq.com>
2026-06-13 09:50:22 +0800

    work: 去掉无用引用

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -335,17 +335,7 @@ export default {
         },
         customBackend: {
           "Mus提供-通用后端": "https://subapi.mus.pp.ua",
-          "CM提供-负载均衡后端": "https://subapi.cmliussss.net",
-          "CM提供-应急备用后端": "https://subapi.fxxk.dedyn.io",
-          "肥羊提供-增强型后端": "https://url.v1.mk",
-          "肥羊提供-备用后端": "https://api.v1.mk",
         },
-        backendOptions: [
-          { value: "https://subapi.cmliussss.net" },
-          { value: "https://subapi.fxxk.dedyn.io" },
-          { value: "https://url.v1.mk" },
-          { value: "https://api.v1.mk" },
-        ],
         remoteConfig: [
           {
             label: "CM规则",
